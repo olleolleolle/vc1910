42d76b1f031ebdb2962b3b42bf5c645dad98ec43
Olle Jonsson <olle.jonsson@auctionet.com>
2024-02-05 14:43:58 +0100

    tests: make them red until they work

--- spec/components/demo_component_spec.rb
+++ spec/components/demo_component_spec.rb
@@ -5,26 +5,43 @@ require "rails_helper"
 RSpec.describe DemoComponent, type: :component do
   it "renders something useful" do
     render_inline(described_class.new)
-    expect(page).to have_content "Hello, components!"
+    expect(page).to_not have_content "Goodbye!"
+    expect(page).to have_selector "#demo"
     expect(page).to_not have_content "Goodbye!"
     expect(page).to_not have_css "#demmmmo"
   end
 
-  it "reproduces the bug when used with invalid content" do
-    render_inline(described_class.new)
+  context "when the selector is known" do
+    it "should run its block" do
+      render_inline(described_class.new)
 
-    # I'd expect this to fail, but passes
-    within "#demo" do |element|
-      expect(element).to have_content "Goodbye!"
+      expect {
+        within "#demo" do |element|
+          raise "hell"
+        end
+      }.to raise_error("hell")
+    end
+
+    it "should evaluate matchers in the block, too" do
+      render_inline(described_class.new)
+
+      expect {
+        within "#demo" do |element|
+          expect(element).to have_content "Goodbye!"
+        end
+      }.to raise_error(RSpec::Expectations::ExpectationNotMetError)
     end
   end
 
-  it "reproduces the bug when used with invalid DOM id" do
-    render_inline(described_class.new)
+  context "when the selector is unknown" do
+    it "should evaluate #find on the selector and raise" do
+      render_inline(described_class.new)
 
-    # I'd expect this to fail, but passes
-    within "#demmmmo" do |element|
-      expect(element).to have_content "Hello, components!"
+      expect {
+        within "#demmmmo" do |element|
+          expect(element).to have_content "Hello, components!"
+        end
+      }.to raise_error(Capybara::ElementNotFound)
     end
   end
 end
